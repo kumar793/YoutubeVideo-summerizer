3c593a2ce3e1074c7ce7ae1ca3dea12749fcd267
NAGENDRA KUMAR VOLETI <vnkumarvoleti@gmail.com>
2025-01-21 12:39:47 +0530

    Update summerizer.py

--- summerizer.py
+++ summerizer.py
@@ -26,7 +26,7 @@ def validate_url(url):
                 if "youtube.com" in url:
                     loader = YoutubeLoader.from_youtube_url(url,add_video_info = True)
                 else:
-                    loader=UnstructuredURLLoader(urls=[generic_url],ssl_verify=False,
+                    loader=UnstructuredURLLoader(urls=[url],ssl_verify=False,
                                                  headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"})
                 
                 data = loader.load()
